7cffc5d52373cc466e3bae47524b943726977229
JMateusSousa <mateus_sousa_peu@hotmail.com>
2021-04-12 14:50:38 -0300

    Adding ML KIT and translating

--- Adri/Controller/SearchMedicineViewController.swift
+++ Adri/Controller/SearchMedicineViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import MLKit
 
 class SearchMedicineViewController: UIViewController, UISearchBarDelegate {
 
@@ -16,10 +17,22 @@ class SearchMedicineViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var textViewContraindication: UITextView!
 
     let managedContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var translator: Translator!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        setDismissKeyboard()
+    }
+
+    func setDismissKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(didTapView))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func didTapView() {
+        view.endEditing(true)
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
@@ -29,30 +42,69 @@ class SearchMedicineViewController: UIViewController, UISearchBarDelegate {
     }
 
     func setInformations(medicine: String) {
-        let url = APIRouters.search.urlMedicine(medicine: medicine)
-        HTTP.get.request(url: url) {  data, response, errorMessage in
-
-            if let errorMessage = errorMessage {
-                print(errorMessage)
-                return
+        var medicineTranslation: String = ""
+        translate(from: "pt", text: medicine, completionHandler: { [weak self] text in
+            guard self != nil else { return }
+            DispatchQueue.main.async {
+                medicineTranslation = text.lowercased()
             }
+        })
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
 
-            guard let data = data, let response = response else {
-                return
-            }
+            let url = APIRouters.search.urlMedicine(medicine: medicineTranslation)
+            print(medicineTranslation)
+            HTTP.get.request(url: url) {  data, response, errorMessage in
 
-            if response.statusCode == 200 {
-                let informations = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                if let results = informations?["results"] as? [[String: Any]] {
-                    let indications = results[0]["indications_and_usage"] as? [String]
-                    let contraindications = results[0]["contraindications"] as? [String]
-                    let textOne = indications!.joined(separator:"-")
-                    let textTwo = contraindications!.joined(separator:"-")
-                    DispatchQueue.main.async {
-                        self.textViewIndication.text = textOne
-                        self.textViewContraindication.text = textTwo
+                if let errorMessage = errorMessage {
+                    print(errorMessage)
+                    return
+                }
+
+                guard let data = data, let response = response else {
+                    return
+                }
+
+                if response.statusCode == 200 {
+                    let informations = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    if let results = informations?["results"] as? [[String: Any]] {
+                        let indications = results[0]["indications_and_usage"] as? [String]
+                        let contraindications = results[0]["contraindications"] as? [String]
+                        let textOne = indications!.joined(separator:"-")
+                        let textTwo = contraindications!.joined(separator:"-")
+
+                        self.translate(from: "en", text: textOne + " | " + textTwo, completionHandler: { [weak self] text in
+                            guard let self = self else { return }
+                            DispatchQueue.main.async {
+                                    let separetedString = text.components(separatedBy: " | ")
+                                    self.textViewIndication.text = separetedString[0]
+                                    self.textViewContraindication.text = separetedString[1]
+                            }
+                        })
                     }
                 }
+            }
+        }
+    }
+
+    private func translate(from: String, text: String, completionHandler: @escaping (String) -> Void) {
+        var options = TranslatorOptions(sourceLanguage: .english, targetLanguage: .portuguese)
+        if from.elementsEqual("pt") {
+            options = TranslatorOptions(sourceLanguage: .portuguese, targetLanguage: .english)
+        }
+
+        let englishPortugueseTranslator = Translator.translator(options: options)
+        let conditions = ModelDownloadConditions(
+            allowsCellularAccess: false,
+            allowsBackgroundDownloading: true
+        )
+
+        englishPortugueseTranslator.downloadModelIfNeeded(with: conditions) { error in
+            guard error == nil else { return }
+
+            // Model downloaded successfully. Okay to start translating.
+            englishPortugueseTranslator.translate(text) { translatedText, error in
+                        guard error == nil, let translatedText = translatedText else { return }
+                completionHandler(translatedText)
             }
         }
     }
---
+++ Adri/View/TableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.swift
+//  Adri
+//
+//  Created by José Mateus Azevedo on 18/08/20.
+//  Copyright © 2020 José Mateus Azevedo. All rights reserved.
+//
+import Foundation
+
+import UIKit
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var medicineName: UILabel!
+    @IBOutlet weak var medicineHour: UILabel!
+}
